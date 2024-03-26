d5719926932f42d3955c8f77f4837005109fa14d
bharathraj <bharathraj16903@gmail.com>
2024-03-26 18:35:11 +0530

    Modify SecondScreen

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/ThirdScreen.dart';
-class SecondScreen extends StatefulWidget {
-  const SecondScreen({super.key});
 
+class SecondScreen extends StatefulWidget {
   @override
   State<SecondScreen> createState() => _SecondScreenState();
 }
@@ -11,68 +9,70 @@ class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Card with rounded bottom corners
-          Positioned(
-            top: 60.0,
-            left: 25.0,
-            right: 25.0,
-            child: Container(
-              height: 450.0,
-              width: 500.0,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
-                    bottomLeft: Radius.circular(10.0),
-                    bottomRight: Radius.circular(10.0),
-                  ), color:Color(0xffEDB2B9)
-
-              ),
-              child:Image.asset("Images/picture2.png"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Positioned(
+              top: 20.0,
+              left: 50.0,
+              right: 50.0,
+              child: Container(
+                height: 500.0,
+                width: 400.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+              )
             ),
-          ),
-          Positioned(
-            top: 550.0, // Adjust based on card height and desired padding
-            left: 25.0,
-            right: 25.0,
-            child: Column(
+            SizedBox(height: 20),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Quick delivery to",
-                  style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "your doorstep",
-                  style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 15.0),
-                Text("We have a dynamic and professional", style: TextStyle(color: Colors.black)),
-                Text( "delivery team that is professionaly and",style: TextStyle(color: Colors.black)),
-                Text( "intelligently trained",style: TextStyle(color: Colors.black)),
-                SizedBox(height: 60.0),
-                //ElevatedButton
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5.0),
                       ),
                       backgroundColor: Color(0xffF53535),
-                      padding: EdgeInsets.symmetric(horizontal: 150.0, vertical: 10.0)
+                      padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 10.0)
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ThirdScreen()),
-                    );
-                  },
-                  child: const Text("Next",style: TextStyle(fontSize: 20,color: Colors.white),textAlign: TextAlign.center),
-                ),
+                  onPressed: () {},
+                  child: const Text("Create a new account",style: TextStyle(fontSize: 20,color: Colors.white),textAlign: TextAlign.start)),
+                SizedBox(height: 20),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: BorderSide(color: Colors.redAccent),
+                        ),
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 150.0, vertical: 10.0)
+                    ),
+                    onPressed: () {},
+                    child: const Text("Sign in",style: TextStyle(fontSize: 20,color: Colors.red),textAlign: TextAlign.start)),
+                SizedBox(height: 20,width: 20,),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: BorderSide(color: Colors.redAccent),
+                        ),
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 10.0)
+                    ),
+                    onPressed: () {},
+                    child: const Text("Continue with Google",style: TextStyle(fontSize: 20,color: Colors.red),textAlign: TextAlign.start)),
               ],
             ),
-          ),
-        ],
+            SizedBox(height: 20,width: 20),
+            Text(
+              'Show more',
+              style: TextStyle(fontSize: 17,fontWeight: FontWeight.w300),
+            ),
+          ],
+        ),
       ),
     );
   }
